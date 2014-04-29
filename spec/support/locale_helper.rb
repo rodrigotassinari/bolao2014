b10b7879ae88f8af39faafc13143a9e41a1ba693
Rodrigo Tassinari de Oliveira <rodrigo@pittlandia.net>
2014-04-29 11:36:47 -0300

    Improves locale test helper syntax

--- spec/support/locale_helper.rb
+++ spec/support/locale_helper.rb
@@ -1,9 +1,9 @@
 RSpec.configure do |config|
-  # pass `i18n: 'pt'` to an example to run it with specific I18n translation
+  # pass `locale: :pt` to an example to run it with specific I18n locale
   config.around(:each) do |example|
-    if example.metadata[:i18n]
+    if example.metadata[:locale]
       default_locale = I18n.default_locale
-      current_locale = example.metadata[:i18n].to_sym
+      current_locale = example.metadata[:locale].to_sym
       raise RuntimeError.new,
         "unavailable locale: '#{current_locale}'" unless I18n.available_locales.include?(current_locale)
       I18n.locale = current_locale
