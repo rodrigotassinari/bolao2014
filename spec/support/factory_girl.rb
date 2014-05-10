18abe5ab381ed51093fb54c5c345ac5fba0d2df4
Rodrigo Tassinari de Oliveira <rodrigo@pittlandia.net>
2014-05-10 15:26:01 -0300

    Checks if all factories are valid automatically

--- spec/support/factory_girl.rb
+++ spec/support/factory_girl.rb
@@ -1,3 +1,13 @@
 RSpec.configure do |config|
   config.include FactoryGirl::Syntax::Methods
+
+  # check if ALL factories are valid
+  config.before(:suite) do
+    begin
+      DatabaseCleaner.start
+      FactoryGirl.lint
+    ensure
+      DatabaseCleaner.clean
+    end
+  end
 end
