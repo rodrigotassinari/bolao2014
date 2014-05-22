3b676d928557447d0d4ffeea4915604fd7ee030b
Rodrigo Tassinari de Oliveira <rodrigo@pittlandia.net>
2014-05-21 22:56:14 -0300

    Action to receive payment notifications

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -2,10 +2,10 @@ class PaymentsController < ApplicationController
 
   skip_before_action :verify_authenticity_token, only: [:update]
   skip_before_action :require_login, only: [:update]
-  before_action :require_guest, except: [:update]
+  before_action :require_guest, only: [:update]
 
-  before_action :find_bet, only: [:new, :create]
-  before_action :find_payment, only: [:new, :create]
+  before_action :find_bet, except: [:update]
+  before_action :find_payment, except: [:update]
 
   # GET /bet/payment
   # Via: bet_payment_path
@@ -30,9 +30,8 @@ class PaymentsController < ApplicationController
   # Via: payment_notifications_path
   #
   # Receives payment notifications from the payment gateway and updates local bet payments.
-  # TODO spec
   def update
-    code = payment_notification_params[:notificationCode]
+    code = params.require(:notificationCode)
     transaction = PagSeguro::Transaction.find_by_notification_code(code)
     if transaction.errors.empty?
       PagseguroTransactionWorker.perform_async(code)
@@ -54,11 +53,6 @@ class PaymentsController < ApplicationController
   def find_payment
     @_payment = Payment.find_or_initialize_with_defaults(@_bet)
     @payment = PaymentPresenter.new(@_payment)
-  end
-
-  def payment_notification_params
-    # params == {"notificationCode"=>"34884C-98D02FD02FCB-500404EF8FA4-15DB27", "notificationType"=>"transaction"}
-    params.require(:notificationCode)
   end
 
 end
--- spec/controllers/payments_controller_spec.rb
+++ spec/controllers/payments_controller_spec.rb
@@ -17,7 +17,53 @@ describe PaymentsController do
   # POST /payment_notifications
   # Via: payment_notifications_path
   describe '#update' do
-    # TODO
+      context 'when logged in' do
+      let(:user) { build(:user) }
+      before(:each) { login_user(user) }
+      it 'returns redirects with an error message' do
+        post :update
+        expect(response).to be_redirect
+        expect(response).to redirect_to(root_path)
+        expect(flash[:notice]).to eql(I18n.t('common.already_logged_in'))
+      end
+    end
+    context 'when not logged in' do
+      let(:params) { {notificationCode: transaction.code, notificationType: 'transaction'} }
+      let(:transaction) { double('PagSeguro::Transaction', code: '34884C-98D02FD02FCB-500404EF8FA4-15DB27', errors: []) }
+      before(:each) do
+        PagSeguro::Transaction.stub(:find_by_notification_code).and_return(transaction)
+      end
+      it 'finds the transaction on PagSeguro' do
+        PagSeguro::Transaction.
+          should_receive(:find_by_notification_code).
+          with(transaction.code).
+          and_return(transaction)
+        post :update, params
+      end
+      context 'with valid PagSeguro params' do
+        it 'queues the payment update from the transaction' do
+          PagseguroTransactionWorker.
+            should_receive(:perform_async).
+            with(transaction.code)
+          post :update, params
+        end
+        it 'responds with an empty success' do
+          post :update, params
+          expect(response).to be_success
+          expect(response.code).to eql('200')
+          expect(response.body).to be_blank
+        end
+      end
+      context 'with invalid PagSeguro params' do
+        let(:transaction) { double('PagSeguro::Transaction', code: '34884C-98D02FD02FCB-500404EF8FA4-15DB27', errors: ['some error']) }
+        it 'responds with an server error' do
+          post :update, params
+          expect(response).to_not be_success
+          expect(response.code).to eql('500')
+          expect(response.body).to be_blank
+        end
+      end
+    end
   end
 
 end
