ddc937df8fb97d273433323dd63a6907de64e200
Marcos Tapajós <tapajos@gmail.com>
2014-05-25 22:01:22 -0300

    Scoring rafactor

--- app/models/match_bet_score.rb
+++ app/models/match_bet_score.rb
@@ -1,11 +1,11 @@
 class MatchBetScore
-  
+
   def self.update(match)
     match.match_bets.find_each do |match_bet|
-      match_bet.points = MatchBetPoints.points(match_bet)
+      match_bet.points = MatchBetPoints.new(match_bet).points
       match_bet.scored_at = Time.zone.now
       match_bet.save!
     end
   end
-  
+
 end
--- spec/models/match_bet_points_spec.rb
+++ spec/models/match_bet_points_spec.rb
@@ -10,42 +10,42 @@ describe MatchBetPoints do
     let(:match_bet) { build(:match_bet, match: match) }
     context 'when all is wrong' do
       it 'scores 0 points' do
-        expect(MatchBetPoints.points(build(:match_bet, match: match, goals_a: 0, goals_b: 1))).to eql(0)
+        expect(MatchBetPoints.new(build(:match_bet, match: match, goals_a: 0, goals_b: 1)).points).to eql(0)
       end
     end
     context 'when all is correct' do
       it 'scores full points' do
-        expect(MatchBetPoints.points(build(:match_bet, match: match, goals_a: 2, goals_b: 0))).to eql(match.total_points)
+        expect(MatchBetPoints.new(build(:match_bet, match: match, goals_a: 2, goals_b: 0)).points).to eql(match.total_points)
       end
     end
     context 'when only goals_a is correct' do
       it 'scores one goals points' do
-        expect(MatchBetPoints.points(build(:match_bet, match: match, goals_a: 2, goals_b: 3))).to eql(match.goal_points * 1)
+        expect(MatchBetPoints.new(build(:match_bet, match: match, goals_a: 2, goals_b: 3)).points).to eql(match.goal_points * 1)
       end
     end
     context 'when only goals_b is correct' do
       it 'scores one goals points' do
-        expect(MatchBetPoints.points(build(:match_bet, match: match, goals_a: 0, goals_b: 0))).to eql(match.goal_points * 1)
+        expect(MatchBetPoints.new(build(:match_bet, match: match, goals_a: 0, goals_b: 0)).points).to eql(match.goal_points * 1)
       end
     end
     context 'when only result is correct' do
       it 'scores result points' do
-        expect(MatchBetPoints.points(build(:match_bet, match: match, goals_a: 3, goals_b: 2))).to eql(match.result_points)
+        expect(MatchBetPoints.new(build(:match_bet, match: match, goals_a: 3, goals_b: 2)).points).to eql(match.result_points)
       end
     end
     context 'when result and goals_a is correct' do
       it 'scores result and one goals points' do
-        expect(MatchBetPoints.points(build(:match_bet, match: match, goals_a: 2, goals_b: 1))).to eql(match.result_points + match.goal_points)
+        expect(MatchBetPoints.new(build(:match_bet, match: match, goals_a: 2, goals_b: 1)).points).to eql(match.result_points + match.goal_points)
       end
     end
     context 'when result and goals_b is correct' do
       it 'scores result and one goals points' do
-        expect(MatchBetPoints.points(build(:match_bet, match: match, goals_a: 3, goals_b: 0))).to eql(match.result_points + match.goal_points)
+        expect(MatchBetPoints.new(build(:match_bet, match: match, goals_a: 3, goals_b: 0)).points).to eql(match.result_points + match.goal_points)
       end
     end
     context 'when goals are corrent but not result' do
       it 'scores two goals points' do
-        expect(MatchBetPoints.points(build(:match_bet, match: finals_match, goals_a: 1, goals_b: 1, penalty_winner_id: team_a.id))).to eql(match.goal_points * 2)
+        expect(MatchBetPoints.new(build(:match_bet, match: finals_match, goals_a: 1, goals_b: 1, penalty_winner_id: team_a.id)).points).to eql(match.goal_points * 2)
       end
     end
   end
