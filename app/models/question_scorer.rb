019b90c53385ca896440664955e3d45d58e2c296
Rodrigo Tassinari de Oliveira <rodrigo@pittlandia.net>
2014-06-08 21:46:16 -0300

    Calculate points for all question_bet on question update

--- app/models/question_scorer.rb
+++ app/models/question_scorer.rb
@@ -3,16 +3,16 @@ class QuestionScorer
 
   attr_reader :question
 
-  # TODO spec
   def initialize(question)
     @question = question
   end
 
   # Score all existing question_bets for this question (calculates the points and saves on the question_bets).
   # Can be run any number of times.
-  # TODO spec
   def score_all!
-    false # TODO
+    question.question_bets.find_each do |question_bet|
+      question_bet.score!
+    end
   end
 
 end
